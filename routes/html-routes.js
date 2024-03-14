ed51aaf4c72f52e53529359f5d2f9182e003b7f6
Rylee Jolley <you@example.com>
2024-03-14 15:22:18 -0600

    added to routes pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,24 @@
-const router = require ('express').Router();
-const path = require ('path');
+const router = require('express').Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-});
+    const indexPath = path.join(__dirname, '../public/index.html');
+    res.sendFile(indexPath, (err) => {
+        if (err) {
+            console.error('Error sending index.html:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});   
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../publix/notes.html'))
+    const indexPath = path.join(__dirname, '../public/notes.html');
+    res.sendFile(indexPath, (err) => {
+        if (err) {
+            console.error('Error sending notes.html:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-const express = require ('express');
-const path = require ('path');
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path');
+const html_routes = require('./routes/html_routes')
+const api_routes = require('./routes/api_routes')
+const PORT = 3001;
+
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+
+app.use(express.static('public'));
+
+app.use(html_routes());
+app.use(api_routes());
 
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
