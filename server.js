37f5e973509b65183783a812cf0b23d21b1614b4
Rylee Jolley <you@example.com>
2024-03-14 16:07:13 -0600

    made changes to the PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const html_routes = require('./routes/html-routes')
 const api_routes = require('./routes/api-routes')
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // set the port
 const app = express();
 
